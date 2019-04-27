12fcefaf8fb91e2d792709291145a7315ec3e3f3
trulepa <trulepa@mail.ru>
2019-04-27 16:29:55 +0400

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,6 @@ function init(event) {
   var $toggle = document.getElementById("main-nav__toggle");
   $toggle.addEventListener("click", handleButtonClick);
 
- /* var $next = document.getElementById("reviews__next");
-  var $prev = document.getElementById("reviews__prev");
-  $next.addEventListener("click", slider);
-  $prev.addEventListener("click", slider); */
-}
-
 function handleButtonClick(event) {
   var $show = document.getElementById("site-list");
   this.classList.toggle("main-nav__toggle--active");
