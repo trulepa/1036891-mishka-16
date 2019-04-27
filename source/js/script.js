86eb2537aa5c30bae2402c614d4626590219206d
trulepa <trulepa@mail.ru>
2019-04-27 16:26:42 +0400

    Добавляет стили странице с формой

--- source/js/script.js
+++ source/js/script.js
@@ -2,19 +2,17 @@
 function init(event) {
   var $toggle = document.getElementById("main-nav__toggle");
   $toggle.addEventListener("click", handleButtonClick);
-  var $next = document.getElementById("reviews__next");
+
+ /* var $next = document.getElementById("reviews__next");
   var $prev = document.getElementById("reviews__prev");
   $next.addEventListener("click", slider);
-  $prev.addEventListener("click", slider);
+  $prev.addEventListener("click", slider); */
 }
 
 function handleButtonClick(event) {
+  var $show = document.getElementById("site-list");
   this.classList.toggle("main-nav__toggle--active");
-  this.nextElementSibling.classList.toggle("site-list--show");
-}
-
-function slider(event) {
-
+  $show.classList.toggle("site-list--show");
 }
 
 window.addEventListener("load", init);
