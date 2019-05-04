87bd27f69d38d97c3912a8e2ccaad7a377d1f079
trulepa <trulepa@mail.ru>
2019-05-05 01:51:00 +0400

    0_o

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,20 @@
+
 function init(event) {
   var $toggle = document.getElementById("main-nav__toggle");
   $toggle.addEventListener("click", handleButtonClick);
+  var $next = document.getElementById("reviews__next");
+  var $prev = document.getElementById("reviews__prev");
+  $next.addEventListener("click", slider);
+  $prev.addEventListener("click", slider);
 }
 
 function handleButtonClick(event) {
-  var $show = document.getElementById("site-list");
   this.classList.toggle("main-nav__toggle--active");
-  $show.classList.toggle("site-list--show");
+  this.nextElementSibling.classList.toggle("site-list--show");
+}
+
+function slider(event) {
+
 }
 
 window.addEventListener("load", init);
